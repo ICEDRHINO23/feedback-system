0fb05a9812705e8ccfd2ac6ccc987cc9fabdcaea
ICEDRHINO23 <abinavft@gmail.com>
2026-06-20 10:19:28 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,13 @@
+function login(){
+    const admissionNo =
+        document.getElementById("admissionNo").value;
 
+    const password =
+        document.getElementById("password").value;
+
+    if(admissionNo && password){
+        window.location.href="dashboard.html";
+    }else{
+        alert("Please enter login details");
+    }
+}
