c1f66ca51e1e3cfb19c679c3bb1cfbd7d0bad265
ICEDRHINO23 <abinavft@gmail.com>
2026-06-23 12:21:35 +0530

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,1 +1,81 @@
 
+import { db } from "./firebase-config.js";
+
+import {
+    doc,
+    getDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+export async function loadClasses(selectId){
+
+    const select =
+        document.getElementById(selectId);
+
+    if(!select) return;
+
+    const configRef =
+        doc(db,"settings","config");
+
+    const configSnap =
+        await getDoc(configRef);
+
+    if(!configSnap.exists()) return;
+
+    const classes =
+        configSnap.data().classes || [];
+
+    select.innerHTML =
+        '<option value="">Select Class</option>';
+
+    classes.forEach(cls => {
+
+        select.innerHTML +=
+        `<option value="${cls}">
+            ${cls}
+        </option>`;
+    });
+}
+
+export async function loadSections(selectId){
+
+    const select =
+        document.getElementById(selectId);
+
+    if(!select) return;
+
+    const configRef =
+        doc(db,"settings","config");
+
+    const configSnap =
+        await getDoc(configRef);
+
+    if(!configSnap.exists()) return;
+
+    const sections =
+        configSnap.data().sections || [];
+
+    select.innerHTML =
+        '<option value="">Select Section</option>';
+
+    sections.forEach(sec => {
+
+        select.innerHTML +=
+        `<option value="${sec}">
+            ${sec}
+        </option>`;
+    });
+}
+
+export async function getSettings(){
+
+    const configRef =
+        doc(db,"settings","config");
+
+    const configSnap =
+        await getDoc(configRef);
+
+    if(!configSnap.exists())
+        return null;
+
+    return configSnap.data();
+}
