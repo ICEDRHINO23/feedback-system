826225342553715f044f2f7420bc36e8871b227a
ICEDRHINO23 <abinavft@gmail.com>
2026-06-23 15:13:32 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,59 @@
 
+import { db } from "./firebase-config.js";
+
+import {
+    collection,
+    getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+async function loadDashboardStats() {
+
+    try {
+
+        // Students
+
+        const studentsSnap =
+            await getDocs(
+                collection(db, "students")
+            );
+
+        document.getElementById(
+            "studentCount"
+        ).innerText =
+            studentsSnap.size;
+
+        // Exams
+
+        const examsSnap =
+            await getDocs(
+                collection(db, "exams")
+            );
+
+        document.getElementById(
+            "examCount"
+        ).innerText =
+            examsSnap.size;
+
+        // Results
+
+        const resultsSnap =
+            await getDocs(
+                collection(db, "results")
+            );
+
+        document.getElementById(
+            "resultCount"
+        ).innerText =
+            resultsSnap.size;
+
+    }
+    catch(error){
+
+        console.error(
+            "Dashboard Error:",
+            error
+        );
+    }
+}
+
+loadDashboardStats();
