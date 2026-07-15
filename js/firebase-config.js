d16677e11c2b888e5c42605a43a93b3d204e348b
ICEDRHINO23 <abinavft@gmail.com>
2026-07-15 12:11:45 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,20 +1,32 @@
-// Firebase Configuration
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+import {
+    getAuth
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDJ7WP7TCT3mZ4WKa20d-b3HSrZ0ZKn0mU",
-  authDomain: "feedbacksys-e3fe0.firebaseapp.com",
-  projectId: "feedbacksys-e3fe0",
-  storageBucket: "feedbacksys-e3fe0.firebasestorage.app",
-  messagingSenderId: "556077863051",
-  appId: "1:556077863051:web:756ce5148c828b3b5e048e",
-  measurementId: "G-P9JZZ3QEFP"
+
+    apiKey: "...",
+
+    authDomain: "...",
+
+    projectId: "...",
+
+    storageBucket: "...",
+
+    messagingSenderId: "...",
+
+    appId: "..."
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const db = getFirestore(app);
+
+const auth = getAuth(app);
+
+export { db, auth };
