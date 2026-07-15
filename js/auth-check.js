eefe49e8075319bbe64c56192b0aab77eeeeccf2
ICEDRHINO23 <abinavft@gmail.com>
2026-07-15 09:47:15 +0530

    Create auth-check.js

---
+++ js/auth-check.js
@@ -0,0 +1,16 @@
+import { auth } from "./firebase-config.js";
+
+import {
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+onAuthStateChanged(auth, (user) => {
+
+    if (!user) {
+
+        alert("Please login first.");
+
+        window.location.href = "login.html";
+    }
+
+});
