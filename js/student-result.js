c1989b11590a4dba6a5589a3cf5d3f82079a7b1f
ICEDRHINO23 <abinavft@gmail.com>
2026-07-30 11:17:13 +0530

    Update student-result.js

--- js/student-result.js
+++ js/student-result.js
@@ -1,1 +1,11 @@
+const score = localStorage.getItem("latestScore") || 0;
+const total = localStorage.getItem("latestTotal") || 0;
 
+document.getElementById("scoreText").innerHTML =
+    `Your Score : ${score}/${total}`;
+
+function goDashboard() {
+    window.location.href = "dashboard.html";
+}
+
+window.goDashboard = goDashboard;
